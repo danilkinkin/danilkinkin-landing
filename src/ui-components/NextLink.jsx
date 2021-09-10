bef8de8940c6d3a4ee338c4351b48dd78f477e0c
Danilkinkin <danilkinkin@gmail.com>
2021-09-10 23:43:25 +0400

    [NextLink] Added startIcon and endIcon

--- src/ui-components/NextLink.jsx
+++ src/ui-components/NextLink.jsx
@@ -1,14 +1,49 @@
 import React, { forwardRef } from 'react';
 import Link from 'next/link';
+import theme from '@/theme';
+import { createUseStyles } from 'react-jss';
+import clsx from 'clsx';
 
-const LinkItem = forwardRef(({ children, ...props }, ref) => {
-    return (<a ref={ref} {...props}>{children}</a>);
+const useStyles = createUseStyles({
+    link: {
+        fontSize: 18,
+        textDecoration: 'unset',
+        fontWeight: 550,
+        color: theme.palette.text.primary,
+        '&:hover': { color: theme.palette.primary },
+    },
+    startIcon: {
+        marginRight: theme.spacing,
+        '& svg': { verticalAlign: 'middle' },
+    },
+    endIcon: {
+        marginLeft: theme.spacing,
+        '& svg': { verticalAlign: 'middle' },
+    },
 });
 
-function NextLink({ children, to, ...props }) {
+const LinkItem = forwardRef(({ children, className, ...props }, ref) => {
+    const classes = useStyles();
+
+    return (<a ref={ref} className={clsx(classes.link, className)} {...props}>{children}</a>);
+});
+
+function NextLink({
+    children, startIcon, endIcon, to, ...props
+}) {
+    const classes = useStyles();
+
     return (
         <Link href={to || '#'} passHref scroll={false}>
-            <LinkItem {...props}>{children}</LinkItem>
+            <LinkItem {...props}>
+                {startIcon && (
+                    <span className={classes.startIcon}>{startIcon}</span>
+                )}
+                {children}
+                {endIcon && (
+                    <span className={classes.endIcon}>{endIcon}</span>
+                )}
+            </LinkItem>
         </Link>
     );
 }
