4b77ed796672d1c397b489956fa6fe96da7a341b
Danilkinkin <danilkinkin@gmail.com>
2021-09-10 23:44:49 +0400

    [navigation] Added mail link in navigation

--- src/ui/Navigation.jsx
+++ src/ui/Navigation.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 import Container from '@/ui-components/Container';
+import NextLink from '@/ui-components/NextLink';
+import MailIcon from '@/icons/Mail';
 
 const useStyles = createUseStyles({
     root: {
         position: 'fixed',
         bottom: 40,
+        left: 0,
+        right: 0,
+        display: 'flex',
+    },
+    nav: {
         maxWidth: 'inherit',
+    },
+    mail: {
+        marginLeft: 'auto',
     },
 });
 
@@ -14,10 +24,17 @@ function Navigation({ children }) {
     const classes = useStyles();
 
     return (
-        <Container>
-            <nav className={classes.root}>
+        <Container className={classes.root}>
+            <nav className={classes.nav}>
                 {children}
             </nav>
+            <NextLink
+                to="mailto:hello@danilkinkin.com"
+                startIcon={(<MailIcon />)}
+                className={classes.mail}
+            >
+                hello@danilkinkin.com
+            </NextLink>
         </Container>
     );
 }
