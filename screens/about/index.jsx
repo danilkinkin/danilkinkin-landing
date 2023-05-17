6723f7e54803fc9dcc020052feb9a23640cac8d9
danilkinkin <d.zahvatkin@ticketscloud.com>
2023-05-18 01:55:43 +0400

    fix: fixed about me block

--- screens/about/index.jsx
+++ screens/about/index.jsx
@@ -1,47 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import Header from '@/components/Header';
-import Container from '@/components/Container';
-import AboutMeBlock from './AboutMeBlock';
-import styles from './About.module.scss';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Header from "@/components/Header";
+import Container from "@/components/Container";
+import AboutMeBlock from "./AboutMeBlock";
+import styles from "./About.module.scss";
 
 function IndexPage() {
-    const router = useRouter();
-    const [path, setPath] = useState([]);
-    const [isFirstRender, setIsFirstRender] = useState(true);
+  const router = useRouter();
+  const [path, setPath] = useState([]);
+  const [isFirstRender, setIsFirstRender] = useState(true);
 
-    useEffect(() => {
-        setIsFirstRender(false);
-        const handleRouteChange = (url /* , { shallow } */) => {
-            if (url === '/') {
-                document.documentElement.scrollTo({
-                    top: 0,
-                    left: 0,
-                    behavior: 'smooth',
-                });
-            } else if (url === '/contacts') {
-                // contacts block
-            } else {
-                router.replace('/');
-                return;
-            }
+  useEffect(() => {
+    setIsFirstRender(false);
+    const handleRouteChange = (url /* , { shallow } */) => {
+      if (url === "/") {
+        document.documentElement.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+      } else if (url === "/contacts") {
+        // contacts block
+      } else {
+        router.replace("/");
+        return;
+      }
 
-            setPath(url);
-        };
+      setPath(url);
+    };
 
-        router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on("routeChangeStart", handleRouteChange);
 
-        return () => {
-            router.events.off('routeChangeStart', handleRouteChange);
-        };
-    }, []);
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
 
-    return (
-        <Container align="left" className={styles.aboutMe}>
-            <AboutMeBlock />
-            <Header />  
-        </Container>     
-    );
+  return (
+    <div className={styles.root}>
+      <Container align="left" className={styles.aboutMe}>
+        <AboutMeBlock />
+      </Container>
+      <Header />
+    </div>
+  );
 }
 
 export default IndexPage;
