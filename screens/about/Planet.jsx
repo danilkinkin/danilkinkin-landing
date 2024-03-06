08762ef51c45b11ab47d049148c79f7b77093fe3
danilkinkin <d.zahvatkin@ticketscloud.com>
2024-03-06 21:58:06 +0500

    fix: fixed three scene

--- screens/about/Planet.jsx
+++ screens/about/Planet.jsx
@@ -3,7 +3,7 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 import styles from "./Planet.module.css";
 import { Fragment } from "react";
-import { useSpring } from '@react-spring/three'
+import { useSpring } from "@react-spring/three";
 import { RectAreaLightUniformsLib } from "three/addons/lights/RectAreaLightUniformsLib";
 import useIsDarkMode from "@/utils/useIsDarkMode";
 
@@ -11,22 +11,28 @@ function Earth() {
   const { nodes, materials } = useGLTF("/earth-transformed.glb");
   const isDarkMode = useIsDarkMode();
   const ref = useRef(null);
-  const [rotationSpeedSpring, rotationSpeedApi] = useSpring(() => ({
+  const [rotationSpeedSpring, rotationSpeedApi] = useSpring(
+    () => ({
       value: 10,
       config: {
         mass: 3,
         friction: 200,
         tension: 600,
       },
-  }), []);
-  const [scaleSpring, scaleApi] = useSpring(() => ({
-    value: 0,
-    config: {
-      mass: 1,
-      friction: 16,
-      tension: 160,
-    },
-}), []);
+    }),
+    [],
+  );
+  const [scaleSpring, scaleApi] = useSpring(
+    () => ({
+      value: 0,
+      config: {
+        mass: 1,
+        friction: 16,
+        tension: 160,
+      },
+    }),
+    [],
+  );
 
   useEffect(() => {
     ref.current.rotation.y = 8.6;
@@ -52,11 +58,47 @@ function Earth() {
 
   return (
     <Fragment>
-      <ambientLight color={ambientColor} intensity={0.5} />
-      <spotLight color={lightColor} position={[0, 20, 0]} lookAt={[0, 0, 0]} intensity={1.3} decay={2} distance={40} penumbra={1} angle={Math.PI/2} />
-      <spotLight color={lightColor} position={[10, 16, 0]} lookAt={[0, 0, 0]} intensity={1.3} decay={2} distance={40} penumbra={1} angle={Math.PI/2} />
-      <spotLight color={lightColor} position={[-10, 16, 0]} lookAt={[0, 0, 0]} intensity={1.3} decay={2} distance={40} penumbra={1} angle={Math.PI/2} />
-      <spotLight color={lightColor} position={[0, 16, 10]} lookAt={[0, 0, 0]} intensity={1.3} decay={2} distance={40} penumbra={1} angle={Math.PI/2} />
+      <ambientLight color={ambientColor} intensity={0.5 * Math.PI} />
+      <spotLight
+        color={lightColor}
+        position={[0, 20, 0]}
+        lookAt={[0, 0, 0]}
+        intensity={1.3 * 400}
+        decay={2}
+        distance={40}
+        penumbra={1}
+        angle={Math.PI / 2}
+      />
+      <spotLight
+        color={lightColor}
+        position={[10, 16, 0]}
+        lookAt={[0, 0, 0]}
+        intensity={1.3 * 400}
+        decay={2}
+        distance={40}
+        penumbra={1}
+        angle={Math.PI / 2}
+      />
+      <spotLight
+        color={lightColor}
+        position={[-10, 16, 0]}
+        lookAt={[0, 0, 0]}
+        intensity={1.3 * 400}
+        decay={2}
+        distance={40}
+        penumbra={1}
+        angle={Math.PI / 2}
+      />
+      <spotLight
+        color={lightColor}
+        position={[0, 16, 10]}
+        lookAt={[0, 0, 0]}
+        intensity={1.3 * 400}
+        decay={2}
+        distance={40}
+        penumbra={1}
+        angle={Math.PI / 2}
+      />
       <group ref={ref} dispose={null}>
         <mesh
           geometry={nodes.Icosphere.geometry}
@@ -75,11 +117,14 @@ function Earth() {
 }
 
 function Planet() {
-
   return (
     <span className={styles.root}>
       <div className={styles.canvasContainer}>
-        <Canvas camera={{ fov: 45, near: 0.1, far: 1000, position: [0, 0, 5] }} dpr={window.devicePixelRatio}>
+        <Canvas
+          camera={{ fov: 45, near: 0.1, far: 1000, position: [0, 0, 5] }}
+          dpr={window.devicePixelRatio}
+          web={true}
+        >
           <Earth />
         </Canvas>
       </div>
