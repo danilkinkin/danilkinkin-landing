c5c0e428963a6bbbbdf7bf332393f3788b5911f8
danilkinkin <d.zahvatkin@ticketscloud.com>
2024-03-06 22:41:09 +0500

    fix: fix page height on safari

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -16,10 +16,10 @@ function MyApp({ Component, pageProps }) {
         <title>{title}</title>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, user-scalable=no"
+          content="width=device-width, height=device-height, initial-scale=1, user-scalable=no, viewport-fit=cover"
         />
         {/* Icons */}
-        <link rel="icon" href="favicon.svg"/>
+        <link rel="icon" href="favicon.svg" />
         {/* Theme */}
         <meta
           name="theme-color"
