b1831e805affaa7e7c54178d7f1e234238c2dde7
danilkinkin <d.zahvatkin@ticketscloud.com>
2026-01-06 01:31:44 +0100

    🚧 added draft home

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -5,8 +5,18 @@ import styles from "./Home.module.css";
 export function Home() {
 	return (
 		<section className={styles.host} id="home">
-			<div />
-			<div>
+			<div className={styles.bentoSection}>
+				<div className={styles.textBlock}>
+					<caption>@danilkinkin</caption>
+					<h1>Web developer & not only;</h1>
+					<p>
+						Work only on ethical, user‑first products. Into code, clean design,
+						and crafting stuff that right
+					</p>
+				</div>
+				<div className={styles.canvas} />
+			</div>
+			<div className={styles.footerSection}>
 				<Navigation current="home" />
 			</div>
 		</section>
