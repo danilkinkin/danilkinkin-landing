dde8cb1c46d711dcc2d1661a1c0c7925f1a80632
danilkinkin <hello@danilkinkin.com>
2026-02-13 14:01:59 +0100

    💫 refactor wind

--- src/modules/forest/WindStreamEffect.tsx
+++ src/modules/forest/WindStreamEffect.tsx
@@ -17,6 +17,8 @@ import {
 } from "three";
 import { Flow } from "three/examples/jsm/modifiers/CurveModifier";
 
+const DEBUG = false;
+
 export function WindStreamEffect() {
 	const { scene, camera } = useThree();
 	const flows =
@@ -41,17 +43,22 @@ export function WindStreamEffect() {
 	};
 	const windStrength = useWind((s) => s.strength);
 
-	const createFlow = (
-		corePoints: Array<[number, number, number]>,
-		layer: number,
-	) => {
-		const start = corePoints[0];
-		const end = corePoints[corePoints.length - 1];
+	const createFlow = ({
+		path,
+		layer,
+		stopPoint = 0.06,
+	}: {
+		path: Array<[number, number, number]>;
+		layer: number;
+		stopPoint?: number;
+	}) => {
+		const start = path[0];
+		const end = path[path.length - 1];
 
 		const points = [
 			new Vector3(start[0] - 5, start[1], start[2]), // buffer before start
-			...corePoints.map(([x, y, z]) => new Vector3(x, y, z)),
-			new Vector3(end[0] + 5, end[1], end[2]), // buffer after end
+			...path.map(([x, y, z]) => new Vector3(x, y, z)),
+			new Vector3(end[0] + 10, end[1], end[2]), // buffer after end
 		];
 		const curve = new CatmullRomCurve3(points, false, "centripetal");
 
@@ -84,7 +91,7 @@ export function WindStreamEffect() {
 		flow.uniforms.pathSegment.value = 1;
 
 		const centerComp = 0.5 * (L / C);
-		flow.uniforms.pathOffset.value = 0.9 - centerComp;
+		flow.uniforms.pathOffset.value = stopPoint - centerComp;
 
 		const debug = new Group();
 
@@ -116,13 +123,29 @@ export function WindStreamEffect() {
 			speedShift,
 			windMesh: mesh,
 			centerComp,
+			stopPoint,
 		};
 	};
 
 	useEffect(() => {
 		const rawFlows = [
-			createFlow(
-				[
+			createFlow({
+				path: [
+					[-2, 0, 0.4],
+					[-1, 0.7, 0.4],
+					[0.7, -1.7, 2.9],
+					[3.8, -3.5, 2.1],
+					[4.3, -1.5, 2.1],
+					[3.2, -0.96, 2.1],
+					[4.7, 0.2, 2.1],
+					[6.8, 0.9, 2.4],
+					[9.6, 1.2, 4.8],
+					[18, 4.5, 3.1],
+				],
+				layer: 8,
+			}),
+			createFlow({
+				path: [
 					[-2, 2.5, 1],
 					[7, 3, 0.9],
 					[9.5, 3.8, 0.9],
@@ -130,24 +153,11 @@ export function WindStreamEffect() {
 					[15, 3.5, 1],
 					[18, 2.8, 1],
 				],
-				1,
-			),
-			createFlow(
-				[
-					[-1.8, 0.5, 0.3],
-					[2, 1.8, 1],
-					[5, 1.3, 0.5],
-					[8, 4.8, 0.5],
-					[9, 5.2, 0.5],
-					[11, 5.4, 0.5],
-					[12.7, 2.8, 3],
-					[15.4, 2.4, 3.7],
-					[18.4, 3.4, 3],
-				],
-				2,
-			),
-			createFlow(
-				[
+				layer: 1,
+				stopPoint: 0.39,
+			}),
+			createFlow({
+				path: [
 					[-2, 1, 2.8],
 					[2.5, -2.5, 2.8],
 					[6, 0.5, 3.2],
@@ -161,43 +171,11 @@ export function WindStreamEffect() {
 					[13, 3, 3.4],
 					[18, 4.9, 3.1],
 				],
-				3,
-			),
-			createFlow(
-				[
-					[-2, 4.5, 0.7],
-					[9, 4.5, 0.7],
-					[11.5, 3.8, 0.7],
-					[14, 4.5, 0.7],
-					[18, 4.7, 1],
-				],
-				4,
-			),
-			createFlow(
-				[
-					[-2, 4, 0.7],
-					[4, 1.7, 0.7],
-					[8.8, 3.2, 0.7],
-					[12, 2.1, 3],
-					[18, 1.5, 4],
-				],
-				5,
-			),
-			createFlow(
-				[
-					[-2, 0.6, 2],
-					[4, 2.8, 1],
-					[8.4, 3.6, 0.6],
-					[10, 1.6, 5.3],
-					[12, 0.8, 5.3],
-					[14, 2.5, 3.6],
-					[15.4, 3.5, 3.1],
-					[18, 0, 3.1],
-				],
-				6,
-			),
-			createFlow(
-				[
+				layer: 3,
+				stopPoint: 0.64,
+			}),
+			createFlow({
+				path: [
 					[-2, -1, 0.6],
 					[2.4, 3, 0.6],
 					[2.85, 2.5, 0.6],
@@ -212,25 +190,22 @@ export function WindStreamEffect() {
 					[15.7, 4.4, 3.1],
 					[18, 4, 3.1],
 				],
-				7,
-			),
-			createFlow(
-				[
-					[-2, 0, 0.4],
-					[-1, 0.7, 0.4],
-					[0.7, -1.7, 2.9],
-					[3.8, -3.5, 2.1],
-					[4.3, -1.5, 2.1],
-					[3.2, -0.96, 2.1],
-					[4.7, 0.2, 2.1],
-					[6.8, 0.9, 2.4],
-					[9.6, 1.2, 4.8],
-					[18, 4.5, 3.1],
+				layer: 7,
+				stopPoint: 0.4,
+			}),
+			createFlow({
+				path: [
+					[-2, 4.5, 0.7],
+					[9, 4.5, 0.7],
+					[11.5, 3.8, 0.7],
+					[14, 4.5, 0.7],
+					[18, 4.7, 1],
 				],
-				8,
-			),
-			createFlow(
-				[
+				layer: 4,
+				stopPoint: 0.56,
+			}),
+			createFlow({
+				path: [
 					[-2, -1.2, 0.7],
 					[7, 8, 0.7],
 					[9.4, 5, 0.7],
@@ -238,21 +213,61 @@ export function WindStreamEffect() {
 					[14, 3.8, 0.7],
 					[18, 3.7, 1],
 				],
-				9,
-			),
+				layer: 9,
+			}),
+			createFlow({
+				path: [
+					[-1.8, 0.5, 0.3],
+					[2, 1.8, 1],
+					[5, 1.3, 0.5],
+					[8, 4.8, 0.5],
+					[9, 5.2, 0.5],
+					[11, 5.4, 0.5],
+					[12.7, 2.8, 3],
+					[15.4, 2.4, 3.7],
+					[18.4, 3.4, 3],
+				],
+				layer: 2,
+				stopPoint: 0.58,
+			}),
+			createFlow({
+				path: [
+					[-2, 0.6, 2],
+					[4, 2.8, 1],
+					[8.4, 3.6, 0.6],
+					[10, 1.6, 5.3],
+					[12, 0.8, 5.3],
+					[14, 2.5, 3.6],
+					[15.4, 3.5, 3.1],
+					[18, 0, 3.1],
+				],
+				layer: 6,
+			}),
+			createFlow({
+				path: [
+					[-2, 4, 0.7],
+					[4, 1.7, 0.7],
+					[8.8, 3.2, 0.7],
+					[12, 2.1, 3],
+					[18, 1.5, 4],
+				],
+				layer: 5,
+				stopPoint: 0.39,
+			}),
 		];
 
 		rawFlows.forEach(({ flow, debug }) => {
 			scene.add(flow.object3D);
-			//scene.add(debug);
+			if (DEBUG) scene.add(debug);
 		});
 
 		flows.current = rawFlows.map(
-			({ flow, speedShift, windMesh, centerComp }, index) => ({
+			({ flow, speedShift, windMesh, centerComp, stopPoint }, index) => ({
 				flow,
 				speedShift,
 				windMesh,
 				centerComp,
+				stopPoint,
 				active: false,
 				delay: Math.random() * 2500 * (index % 3),
 			}),
@@ -261,7 +276,7 @@ export function WindStreamEffect() {
 		return () => {
 			rawFlows.forEach(({ flow, debug }) => {
 				scene.remove(flow.object3D);
-				//scene.remove(debug);
+				if (DEBUG) scene.remove(debug);
 			});
 		};
 	}, []);
@@ -283,6 +298,11 @@ export function WindStreamEffect() {
 
 				if (flow.delay > 0) return;
 
+				console.log(
+					"delta:",
+					-0.035 * delta * windStrengthSmooth.current * flow.speedShift,
+				);
+
 				flow.flow.uniforms.pathOffset.value +=
 					-0.035 * delta * windStrengthSmooth.current * flow.speedShift;
 
@@ -290,12 +310,18 @@ export function WindStreamEffect() {
 					flow.flow.uniforms.pathOffset.value = 0.95 - flow.centerComp;
 					flow.speedShift = (Math.random() + 0.5) * windEffect.randomizeSpeed;
 					flow.delay = Math.random() * 500;
+				}
 
-					if (windStrength === 0) {
-						flow.active = false;
-						flow.delay = Math.random() * 2500 * (index % 3);
-						stopped += 1;
-					}
+				if (
+					windStrength === 0 &&
+					flow.flow.uniforms.pathOffset.value + flow.centerComp <=
+						flow.stopPoint + 0.005 &&
+					flow.flow.uniforms.pathOffset.value + flow.centerComp >=
+						flow.stopPoint - 0.005
+				) {
+					flow.active = false;
+					flow.delay = Math.random() * 2500 * (index % 3);
+					stopped += 1;
 				}
 			}
 		});
